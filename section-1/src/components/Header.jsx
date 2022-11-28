83f69dbf7e1fa7a9a8ed6a449720fc1a68b0a083
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-11-28 14:02:28 +0500

    Project structure

---
+++ section-1/src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  const getTheYear = () => {
+    const newDate = new Date();
+    return newDate.getFullYear;
+  };
+
+  return (
+    <>
+      <div>The date is {getTheYear()}</div>
+    </>
+  );
+};
+
+export default Header;
--- section-1/src/index.js
+++ section-1/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div className='hey'>
-      <h1>Hello 🚚</h1>
+      <Header />
     </div>
   );
 };
