d7bb905c03795691754804433d44de157ba6e1d1
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-12-02 19:27:22 +0500

    Props and functions

--- section-1/src/components/Header.jsx
+++ section-1/src/components/Header.jsx
@@ -1,39 +1,18 @@
-import React, { Component } from "react";
-
-class Header extends Component {
-  state = {
-    active: false,
-    keywords: "",
-  };
-
-  inputChange = (e) => {
-    this.setState({
-      keywords: e.target.value,
-    });
-  };
-
-  changeColor = () => {
-    this.setState({
-      active: this.state.active ? false : true,
-    });
-  };
-
-  render() {
-    return (
-      // <header style={{ background: `${this.state.active ? "red" : "blue"}` }}>
-      <header className={this.state.active ? "active" : "not-active"}>
-        <div className='logo'>Logo</div>
-        <input type='text' onChange={(e) => this.inputChange(e)} />
-        <button
-          onClick={() => {
-            this.changeColor();
-          }}
-        >
-          Change it
-        </button>
-      </header>
-    );
-  }
-}
+const Header = (props) => {
+  return (
+    // <header style={{ background: `${this.state.active ? "red" : "blue"}` }}>
+    <header className={props.active ? "active" : "not-active"}>
+      <div className='logo'>Logo</div>
+      <input type='text' onChange={props.keyword} />
+      <button
+        onClick={() => {
+          props.changeColor();
+        }}
+      >
+        Change it
+      </button>
+    </header>
+  );
+};
 
 export default Header;
--- section-1/src/index.js
+++ section-1/src/index.js
@@ -1,31 +1,56 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './components/Header';
-import NewsList from './components/NewsList';
-import Footer from './components/Footer';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import NewsList from "./components/NewsList";
+import Footer from "./components/Footer";
 
-import './styles/style.css';
-import JSON from './db.json';
+import "./styles/style.css";
+import JSON from "./db.json";
 
 class App extends Component {
   state = {
+    active: false,
     news: JSON,
-    footerText: 'I am a happy footer'
+    filtered: [],
+    footerText: "I am a happy footer",
+  };
+
+  getKeywords = (event) => {
+    console.log(event.target.value);
+    let keywords = event.target.value;
+    let filtered = this.state.news.filter((item) => {
+      return item.title.indexOf(keywords) > -1;
+    });
+    this.setState({
+      filtered,
+    });
+  };
+
+  changeColor = () => {
+    this.setState({
+      active: this.state.active ? false : true,
+    });
   };
 
   render() {
+    const { active, filtered, news, footerText } = this.state;
+
     return (
       <div className='hey'>
-        <Header />
-        <NewsList news={this.state.news} >
+        <Header
+          active={active}
+          changeColor={this.changeColor}
+          keyword={this.getKeywords}
+        />
+        <NewsList news={filtered.length === 0 ? news : filtered}>
           <br />
           <h1>I am a children</h1>
         </NewsList>
-        <Footer footerText={this.state.footerText} />
+        <Footer footerText={footerText} />
       </div>
     );
   }
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
