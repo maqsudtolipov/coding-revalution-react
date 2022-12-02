8a6ae08867bc8fd9ecddaec065d2c3258a3c4377
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-12-02 18:46:17 +0500

    Dynamic styles

--- section-1/src/components/Header.jsx
+++ section-1/src/components/Header.jsx
@@ -1,22 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
   state = {
-    keywords: '',
+    active: false,
+    keywords: "",
   };
 
   inputChange = (e) => {
     this.setState({
       keywords: e.target.value,
     });
-    console.log(e.target.value);
+  };
+
+  changeColor = () => {
+    this.setState({
+      active: this.state.active ? false : true,
+    });
   };
 
   render() {
     return (
-      <header>
+      // <header style={{ background: `${this.state.active ? "red" : "blue"}` }}>
+      <header className={this.state.active ? "active" : "not-active"}>
         <div className='logo'>Logo</div>
         <input type='text' onChange={(e) => this.inputChange(e)} />
+        <button
+          onClick={() => {
+            this.changeColor();
+          }}
+        >
+          Change it
+        </button>
       </header>
     );
   }
