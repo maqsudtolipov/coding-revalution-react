9a7f7e3a53c91035d5b6de8988ee9bf4e408f360
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-12-04 23:53:47 +0500

    useState hook part 2

--- 2-hooks/src/App.js
+++ 2-hooks/src/App.js
@@ -2,26 +2,58 @@ import { useState } from "react";
 import "./app.css";
 
 function App(props) {
-  const [count, setCount] = useState(props.initialCount);
+  const [state, setState] = useState({
+    count: props.initialCount,
+    user: "Franklin",
+  });
+
+  const [posts, setPosts] = useState([
+    {
+      name: "Super awsome hooks",
+      body: "Everything you need for your hooks",
+    },
+  ]);
 
   // const addOne = () => {
   //   setCount(count + 1);
   // };
 
   const minusOne = () => {
-    setCount((prevCount) => prevCount - 1);
+    setState((prevState) => {
+      return { ...prevState, count: prevState.count - 1 };
+    });
   };
 
-  const reset = () => {
-    setCount(props.initialCount);
-  }
+  const addOnePost = () => {
+    let newPost = {
+      name: "🍬 is bad",
+      body: "☕ is good",
+    };
+
+    setPosts([...posts, newPost]);
+  };
 
   return (
     <div>
-      <h3>Count: {count}</h3>
-      <button onClick={() => setCount(count + 1)}>Plus one</button>
+      <h1>{state.user}</h1>
+      <h3>Count: {state.count}</h3>
+      <button onClick={() => setState({ ...state, count: state.count + 1 })}>
+        Plus one
+      </button>
       <button onClick={minusOne}>Minus one</button>
-      <button onClick={reset}>Reset</button>
+      <button onClick={() => setState({ ...state, count: props.initialCount })}>
+        Reset
+      </button>
+
+      <hr />
+      {posts.map((post, i) => (
+        <div>
+          <div>Name: {post.name}</div>
+          <div>Body: {post.body}</div>
+          <hr />
+        </div>
+      ))}
+      <button onClick={addOnePost}>Add one more</button>
     </div>
   );
 }
