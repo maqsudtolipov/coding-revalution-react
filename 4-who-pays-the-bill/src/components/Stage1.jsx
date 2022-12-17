eff8f7d093283be5e4f86c37c461712f679fe601
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-12-17 22:41:52 +0500

    Add players to the list

--- 4-who-pays-the-bill/src/components/Stage1.jsx
+++ 4-who-pays-the-bill/src/components/Stage1.jsx
@@ -1,13 +1,36 @@
-import { Button, Input } from 'dracula-ui';
+import { Button, Input, Text } from 'dracula-ui';
+import { useState, useContext } from 'react';
 import { useRef } from 'react';
+import { MyContext } from '../context';
 
 const Stage1 = () => {
+  const context = useContext(MyContext);
+
+  const [error, setError] = useState([false, '']);
+
   const textInput = useRef();
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(textInput.current.value);
     const value = textInput.current.value;
+    const validate = validateInput(value);
+
+    if (validate) {
+      setError([false, '']);
+      context.addPlayer(value);
+      textInput.current.value = '';
+    }
+  };
+
+  console.log(context.state);
+
+  const validateInput = (value) => {
+    if (value === '' || value.length < 3) {
+      setError([true, 'Please enter a valid value']);
+      return false;
+    }
+
+    return true;
   };
 
   return (
@@ -20,6 +43,10 @@ const Stage1 = () => {
           mt='xs'
           ref={textInput}
         />
+
+        {error[0] ? <Text>{error[1]}</Text> : null}
+        <br />
+
         <Button type='submit' color='yellow' mt='sm'>
           Add
         </Button>
--- 4-who-pays-the-bill/src/context/index.js
+++ 4-who-pays-the-bill/src/context/index.js
@@ -5,6 +5,13 @@ const MyContext = React.createContext();
 class MyProvider extends React.Component {
   state = {
     stage: 1,
+    players: [],
+  };
+
+  addPlayer = (name) => {
+    this.setState((prevState) => ({
+      players: [...prevState.players, name],
+    }));
   };
 
   render() {
@@ -13,6 +20,7 @@ class MyProvider extends React.Component {
         <MyContext.Provider
           value={{
             state: this.state,
+            addPlayer: this.addPlayer,
           }}
         >
           {this.props.children}
