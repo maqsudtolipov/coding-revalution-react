eb543021d2ae8cc1ef2d6cda8bf67fb3550cb207
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-12-17 21:47:56 +0500

    Creating the form

--- 4-who-pays-the-bill/src/components/Stage1.jsx
+++ 4-who-pays-the-bill/src/components/Stage1.jsx
@@ -1,5 +1,31 @@
+import { Button, Input } from 'dracula-ui';
+import { useRef } from 'react';
+
 const Stage1 = () => {
-  return <>Stage 1</>;
+  const textInput = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(textInput.current.value);
+    const value = textInput.current.value;
+  };
+
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <Input
+          type='text'
+          placeholder='Add player name'
+          color='green'
+          mt='xs'
+          ref={textInput}
+        />
+        <Button type='submit' color='yellow' mt='sm'>
+          Add
+        </Button>
+      </form>
+    </>
+  );
 };
 
 export default Stage1;
