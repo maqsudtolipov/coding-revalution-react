76c782f3dee7dbe4fb6d3575fcb42f5ae770a777
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-12-21 16:50:38 +0500

    Using routes

--- 5-routes/src/App.js
+++ 5-routes/src/App.js
@@ -1,5 +1,10 @@
-import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { NextUIProvider, createTheme, Container } from '@nextui-org/react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Posts from './components/Posts';
+import './App.css';
+import Profile from './components/Profile';
+import Home from './components/Home';
 
 const theme = createTheme({
   type: 'dark', // light / dark
@@ -23,7 +28,19 @@ const theme = createTheme({
 });
 
 function App() {
-  return <NextUIProvider theme={theme}>a</NextUIProvider>;
+  return (
+    <NextUIProvider theme={theme}>
+      <Container>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='posts' element={<Posts />} />
+            <Route path='profile' element={<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </NextUIProvider>
+  );
 }
 
 export default App;
--- 5-routes/src/components/Home.jsx
+++ 5-routes/src/components/Home.jsx
@@ -1,5 +1,11 @@
+import { Text } from '@nextui-org/react';
+
 const Home = () => {
-  return ( <>Home</> );
-}
- 
+  return (
+    <>
+      <Text  size="$2xl">Home</Text>
+    </>
+  );
+};
+
 export default Home;
