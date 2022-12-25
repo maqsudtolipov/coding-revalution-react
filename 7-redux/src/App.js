08425ee6ba225ec92fd25c863ea4040d0e9ed610
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-12-25 20:44:18 +0500

    Redux actions

--- 7-redux/src/App.js
+++ 7-redux/src/App.js
@@ -1,7 +1,49 @@
 import './App.css';
+import img from './assets/popcorn.png';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Text } from '@nextui-org/react';
+import { addMovie } from './store/movies';
+import { setType } from './store/users';
 
 function App() {
-  return <div>App</div>;
+  const movies = useSelector((state) => state.movies.list);
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  return (
+    <div className='app'>
+      <Text h2>Movies</Text>
+      <ul>
+        {movies &&
+          movies.map((movie) => (
+            <li
+              key={movie.id}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                src={img}
+                style={{
+                  height: '24px',
+                  marginRight: '12px',
+                }}
+              />{' '}
+              {movie.title}
+            </li>
+          ))}
+      </ul>
+      <br />
+      <Button onClick={() => dispatch(addMovie({ id: 3, title: 'Batman' }))}>
+        Add Movie
+      </Button>
+      <br />
+      <Text h3>User type: {users.type}</Text>
+      <Button onClick={() => dispatch(setType('Admin'))}>Set Type</Button>
+    </div>
+  );
 }
 
 export default App;
---
+++ 7-redux/src/store/movies.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const moviesSlice = createSlice({
+  name: 'movies',
+  initialState: {
+    list: [
+      { id: 1, title: 'Pulp fiction' },
+      { id: 2, title: 'Rambo' },
+    ],
+  },
+  reducers: {
+    addMovie: (state, action) => {
+      // const newMovie = { id: 3, title: 'Batman' };
+
+      state.list = [...state.list, action.payload];
+    },
+  },
+});
+
+export const { addMovie } = moviesSlice.actions;
+export default moviesSlice.reducer;
